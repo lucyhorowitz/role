669137ea1f53133c940ba5f9a5a9012782a0e911
Lucy Horowitz <111657652+lucyhorowitz@users.noreply.github.com>
2025-06-12 10:26:38 -0700

    remove hotdog

--- hotdog.py
+++
@@ -1,9 +0,0 @@
-# The hotdog example, where we have three atomic propositions:
-#   - `a`, representing "eat hotdog"
-#   - `b`, representing "ride rollercoaster"
-#   - `c`, representing "happy".
-# We put the following in our material base: 
-#   - hotdog |~ happy
-#   - rollercoaster |~ happy
-#   - hotdog, rollercoaster |/~ happy
-# We also stipulate that 
